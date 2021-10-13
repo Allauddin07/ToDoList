82e5bd84e108cddef30f066965c7ba09be8679dd
Allauddin07 <66093421+Allauddin07@users.noreply.github.com>
2021-10-13 21:37:42 +1300

    first commit

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/', ( req, res)=>{
+    res.send("hello world")
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening to ${PORT}`)
+})
